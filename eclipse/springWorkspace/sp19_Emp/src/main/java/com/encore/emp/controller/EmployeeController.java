ccab46c80856f9107df57caa68812be8ca719577
Lily <lily2138@users.noreply.github.com>
2020-08-20 17:41:54 +0900

    vue랑 spring이랑 연결

--- eclipse/springWorkspace/sp19_Emp/src/main/java/com/encore/emp/controller/EmployeeController.java
+++ eclipse/springWorkspace/sp19_Emp/src/main/java/com/encore/emp/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins= {"*"}, maxAge=6000)    //vue랑 연결
 @Api(tags= {"Encore HRM"})
 public class EmployeeController {
 
